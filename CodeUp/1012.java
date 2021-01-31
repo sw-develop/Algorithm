b77b948aad5d91f5875cce3da0d56437f6dd0c76
sw-develop <psw00719@naver.com>
2021-01-31 03:30:54 +0000

    [ADD]

---
+++ CodeUp/1012.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {  
+  public static void main(String args[]) { 
+    Scanner kbd = new Scanner(System.in);
+    double val = kbd.nextDouble();
+    System.out.printf("%6f", val);
+  } 
+}

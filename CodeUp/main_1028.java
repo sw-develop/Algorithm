9808603fe9bcbcbc50d12bde31d71228a9383ca6
sw-develop <psw00719@naver.com>
2021-02-02 17:12:09 +0000

    기초 데이터형

---
+++ CodeUp/main_1028.java
@@ -0,0 +1,13 @@
+//정수 1개 입력받아 그대로 출력하기2
+//입력되는 정수의 범위는 0 ~ 4,294,967,295
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class main_1028{
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println(br.readLine());
+  }
+}

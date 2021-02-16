07b2d2e8af6a99d7713f6d40d17606d9234b65a2
sw-develop <psw00719@naver.com>
2021-02-16 01:22:51 +0000

    apply .gitignore

--- CodeUp/1012.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class Main {  
-  public static void main(String args[]) { 
-    Scanner kbd = new Scanner(System.in);
-    double val = kbd.nextDouble();
-    System.out.printf("%6f", val);
-  } 
-}

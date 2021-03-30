9ceceaaea80d39ee9c338791f2564d955fecd83e
sw-develop <psw00719@naver.com>
2021-03-30 01:14:49 +0000

    수정

--- BOJ/Main_10818.java
+++ BOJ/Main_10818.java
@@ -6,19 +6,17 @@ public class Main_10818 {
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int count = Integer.parseInt(br.readLine());
+    int[] arr = new int[Integer.parseInt(br.readLine())]; //배열 선언 및 생성 
 
-    StringTokenizer st = new StringTokenizer(br.readLine(), " "); 
+    StringTokenizer st = new StringTokenizer(br.readLine(), " "); //문자열을 토큰으로 분리
+    for(int i = 0; i<arr.length; i++)
+      arr[i] = Integer.parseInt(st.nextToken());
+    int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
 
-    int max = Integer.parseInt(st.nextToken());
-    int min = max;
-    int val = 0;
-
-    while(st.hasMoreTokens()){
-      val = Integer.parseInt(st.nextToken());
-      if(val > max) max = val;
-      else if(val < min) min = val;
-    }
+   for(int i = 0; i<arr.length; i++){
+     if(arr[i] > max) max = arr[i];
+     if(arr[i] < min) min = arr[i];
+   }
 
     System.out.printf("%d %d", min, max);
 }

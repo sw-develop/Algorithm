6b9b84532a929dafdb4611e152b138b48300bc7c
sw-develop <psw00719@naver.com>
2021-06-29 01:55:33 +0000

    greedy

--- Programmers/greedy/Gymsuit.java
+++ Programmers/greedy/Gymsuit.java
@@ -1,66 +1,31 @@
 //주어진 배열들이 정렬되어 있지 않다
-import java.util.Arrays;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         //lost: 잃어버린 학생 번호 배열, reserve: 여벌 옷 가지고 있는 학생 번호 배열 
-        int answer = 0;
+        //lost와 reserve를 합쳐서 하나의 배열로 생각하기!
+        int stu[] = new int[n];
+        int answer = n; //전체 학생 수로 설정해두고 감소시키기 -> 증가시키는 경우보다 코드가 적어짐 
         
-        answer = n - lost.length; //체육복 도난당하지 않은 애들 
-        
-        //주어진 lost, reserve 배열 오름차순 정렬하기 
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        //여벌 옷 가지고 있는 학생 중 체육복을 잃어버린 학생 찾기
-        int j = 0, k = 0, i = 0;
-        for(i = 0; i < lost.length; i++){
-            for(j=k; j<reserve.length; j++){
-                if(lost[i] == reserve[j]){
-                    reserve[j] = 0; 
-                    lost[i] = 0;
-                    answer++; 
-                    k = j+1; //k값 재설정 
+        for(int i : lost)
+            stu[i-1]--;
+        for(int j : reserve)
+            stu[j-1]++;
+            
+        //그리디 문제인 이유 : 앞뒤 사람 모두 여유복을 가지고 있을 때, 앞 사람의 것을 빌려야 최대가 나옴
+        for(int k = 0; k < stu.length; k++){
+            if(stu[k] == -1){ //체육복을 도난당한 학생의 경우
+                if(k-1 >= 0 && stu[k-1] == 1){ //앞 사람 먼저 확인  
+                    stu[k]++;
+                    stu[k-1]--;
                 }
-                if(reserve[j] == 0) break; 
+                else if(k+1 < stu.length && stu[k+1] == 1){
+                    stu[k]++;
+                    stu[k+1]--;
+                }
+                else answer--; //체육수업을 듣지 못하는 학생 
             }
         }
-        
-        //앞에 번호 애꺼를 빌리기
-        int index1=0, index2=0; //lost와 reserve의 인덱스
-        int check = 0; //빌리기 성공하면 1
-        while(index1 < lost.length){ 
-            int lost_num = lost[index1];
-        
-            if(lost_num == 0){
-                index1++;
-                continue;
-            }
-            
-            for(i = index2; i < reserve.length; i++){
-                if(reserve[i] == 0) {
-                    continue; //다음 반복으로 
-                }
-                if(lost_num > reserve[i])
-                    if(reserve[i] == lost_num-1) check = 1;
-                else if(lost_num < reserve[i])
-                    if(reserve[i] == lost_num+1) check = 1;
-                else check = 1;
-                
-                if(check == 1){
-                    answer++;
-                    index2 = i+1; //다음번 reserve 배열 값 체크 위치 
-                    break;
-                }
-            }
-            
-            
-            if(check == 1) check = 0; //check 값 초기화 
-            
-            index1++;
-        }
-        
-        
         return answer;
     }
 }

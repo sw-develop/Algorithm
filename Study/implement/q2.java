103f7abeee6daa0eecba593631abf64ece064e2a
sw-develop <psw00719@naver.com>
2021-07-03 22:59:05 +0000

    Update Code

--- Study/implement/q2.java
+++ Study/implement/q2.java
@@ -1,48 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
+	public static int lookAt; 
 	
-	public static void whereCanImove(int lookAt, int moveRow[], int moveCol[]) {
-		if(lookAt == 0) { //북쪽 
-			moveRow[0] = 0;
-			moveRow[1] = 1;
-			moveRow[2] = 0;
-			moveCol[0] = -1;
-			moveCol[1] = 0;
-			moveCol[2] = 1;
-		}
-		else if(lookAt == 1) { //동쪽 
-			moveRow[0] = -1;
-			moveRow[1] = 0;
-			moveRow[2] = 1;
-			moveCol[0] = 0;
-			moveCol[1] = -1;
-			moveCol[2] = 0;
-		}
-		else if(lookAt == 2) { //남쪽 
-			moveRow[0] = 0;
-			moveRow[1] = -1;
-			moveRow[2] = 0;
-			moveCol[0] = 1;
-			moveCol[1] = 0;
-			moveCol[2] = -1;
-		}
-		else { //서쪽 
-			moveRow[0] = 1;
-			moveRow[1] = 0;
-			moveRow[2] = -1;
-			moveCol[0] = 0;
-			moveCol[1] = 1;
-			moveCol[2] = 0;
-		}
+	public static void turnLeft() { //개선2 
+		lookAt -= 1;
+		if(lookAt == -1) lookAt = 3; //북 -> 서 방향 전환 시 
 	}
-	
 
 	public static void main(String[] args) {
 		Scanner kbd = new Scanner(System.in);
 		
 		int N, M; //직사각형 크기 
-		int x, y, lookAt; //사용자 위치, 바로보고 있는 방향 
+		int x, y; //사용자 위치, 바로보고 있는 방향 
 		
 		N = kbd.nextInt();
 		M = kbd.nextInt();
@@ -51,81 +21,60 @@ public class Main {
 		y = kbd.nextInt();
 		lookAt = kbd.nextInt(); //바라보고 있는 방향  
 		
-		int gameBoard[][] = new int[N][M];
+		int gameBoard[][] = new int[N][M]; //게임맵 
 		for(int i = 0; i < N; i++)
 			for(int j = 0; j < M; j++)
 				gameBoard[i][j] = kbd.nextInt();
-		
-		
-		int moveRow[] = new int[3]; //바라보는 방향에 따라 이동 가능한 row 값 저장 배열 
-		int moveCol[] = new int[3]; //바라보는 방향에 따라 이동 가능한 col 값 저장 배열 
 		
 		//직관적으로 변수명 표현 ! 
 		int visited = 2;
 		int ocean = 1;
 		int land = 0;
 		
-		int dx = 0;
-		int dy = 0;
+		gameBoard[x][y] = visited; //현재 사용자 위치 방문했다고 표시
 		
-		boolean moveSuccess = false; 
+		//북 동 남 서에 따른 이동 거리 -> 개선1 
+		int dx[] = {-1, 0, 1, 0}; 
+		int dy[] = {0, 1, 0, -1}; 
 		
 		int moveCount = 0; 
+		int turnTime = 0; 
+		 
 		
-		gameBoard[x][y] = visited; //현재 사용자 위치 방문했다고 표시 
-		
+		//게임 룰 순서대로 처리해주기 
 		while(true) {
+			//1. 왼쪽 회전
+			turnLeft();
 			
-			System.out.println("game start!");
+			int nx = x + dx[lookAt];
+			int ny = y + dy[lookAt];
 			
-			moveSuccess = false; //초기화 -> 놓친 부분1! 
-			
-			whereCanImove(lookAt, moveRow, moveCol);
-			
-			for(int i = 0; i < 3; i++) {
-				if(lookAt-1 < 0) lookAt = 3; //북쪽일 때 처리 -> 놓친 부분2!
-				else lookAt--;
-				
-				dx = x + moveRow[i]; 
-				dy = y + moveCol[i];
-				
-				if(dx != -1 && dx != M && dy != -1 && dy != N) { //게임 맵 범위를 벗어나지 않았을 때 
-					if(gameBoard[dx][dy] == land) { //아직 가보지 않은 칸이 존재할 때 
-						x = dx;
-						y = dy;
-						moveSuccess = true;
-						moveCount++;
-						gameBoard[x][y] = visited;
-					}
-				}
-				
-				if(moveSuccess) break;
+			//2-1. 회전 이후 정면에 가보지 않은 칸이 존재하는 경우 
+			if(gameBoard[nx][ny] == land) {
+				gameBoard[nx][ny] = visited; //방문 표시
+				x = nx; //사용자 위치 업데이트 
+				y = ny; 
+				moveCount++;
+				turnTime = 0;
+				continue; //밑의 조건들 확인 필요 없으니까 
 			}
+			//2-2. 회전 이후 정면에 가보지 않은 칸이 없거나 바다인 경우 
+			else turnTime++; 
 			
-			if(!moveSuccess) {
-				if(lookAt-1 < 0) lookAt = 3; //북쪽일 때 처리 
-				else lookAt--; 
-				
-				//바라보는 방향 따라 달라짐 -> 한 칸 뒤로 이동 
-				if(lookAt == 0) { //북 
-					if(x+1 != M) x = x+1;
-				}
-				else if(lookAt == 1) { //동 
-					if(y-1 != -1) y = y-1;
-				}
-				else if(lookAt == 2) { //남 
-					if(x-1 != -1) x = x-1;
-				}
-				else { //서 
-					if(y+1 != N) y = y+1;
-				}
-				
-				if(gameBoard[x][y] == ocean) //이동한 곳이 바다일 때 
-					break;
-				else 
+			//3. 네 방향 모두 갈 수 없는 경우
+			if(turnTime == 4) {
+				nx = x - dx[lookAt];
+				ny = y - dx[lookAt]; 
+				//뒤로 갈 수 있는 경우 뒤로 이동하기 
+				if(gameBoard[nx][ny] != ocean) {
+					x = nx; //사용자 위치 업데이트 
+					y = ny; 
 					moveCount++;
+				}
+				//뒤가 바다인 경우 이동 불가 
+				else break; 
+				turnTime = 0; //회전 횟수 초기화 
 			}
-			
 		}
 		
 		System.out.println(moveCount);

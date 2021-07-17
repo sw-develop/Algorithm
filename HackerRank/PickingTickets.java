e4427e9c71ff611d591724fb114c0b75306aa09a
sw-develop <psw00719@naver.com>
2021-07-17 08:13:16 +0000

    [FIX]

--- HackerRank/PickingTickets.java
+++ HackerRank/PickingTickets.java
@@ -1,21 +1,24 @@
 // Complete the maxTickets function below.
 //부분배열의 최대 크기 반환!
 static int maxTickets(List<Integer> tickets) {
-int maxArrSize = 0; //배열 크기 반환 Collections.sort(tickets);
+	 int maxSubSequenceSize = 0;
+	 
+     Collections.sort(tickets); //주어진 리스트 오름차순 정렬 
+     
      int beforeVal = 0;
-     int beforeArrSize = 1;
-     beforeVal = tickets.get(0);
+     int subSequenceSize = 1;
+     beforeVal = tickets.get(0); //리스트의 첫 번째 원소부터 시작 
      for(int i = 1; i < tickets.size(); i++){
-         int difference = tickets.get(i) - beforeVal;
+         int difference = tickets.get(i) - beforeVal; //j+1 - j
          beforeVal = tickets.get(i);
-         if(difference == 0 || difference == 1) {
-             beforeArrSize++;
+         if(difference <= 1) { //주어진 조건 만족 시
+             beforeArrSize++; //부분배열 크기 증가 
          }
-         else if(difference != 0 && difference != 1) {
-             if(maxArrSize < beforeArrSize)
-                 maxArrSize = beforeArrSize;
-             beforeArrSize = 1;
+         else {
+             if(maxSubSequenceSize < subSequenceSize)
+                 maxSubSequenceSize = subSequenceSize; //부분 배열 최대 크기 업데이트 
+             subSequenceSize = 1; //부분 배열 크기 초기화 
          }
      }
-     return maxArrSize;
+     return maxSubSequenceSize;
 }

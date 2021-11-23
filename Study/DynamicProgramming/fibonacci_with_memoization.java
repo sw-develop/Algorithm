b7947db75c938c5100766d6a47e2c582444be70a
sw-develop <psw00719@naver.com>
2021-11-23 10:53:27 +0000

    Add: 메모이제이션 기법을 사용한 피보나치 수열

---
+++ Study/DynamicProgramming/fibonacci_with_memoization.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class fibonacci_with_memoization {
+
+  // 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 배열 초기화 
+  public static long[] result = new long[100];
+
+  // 피보나치 함수를 재귀함수로 구현 (with 탑다운 다이나믹 프로그래밍)
+  public static long fibo(int x){
+    if(x == 1 || x == 2){ // 재귀 종료 조건
+      return 1;
+    }
+
+    if(result[x] != 0){ // 이미 계산한 적이 있는 값이라면 배열의 값 그대로 반환 
+      return result[x];
+    }
+
+    result[x] = fibo(x-1) + fibo(x-2);  // 계산한 적이 없는 겂이라면 점화식에 따라 피보나치 결과 반환 
+    return result[x];
+  }
+
+  public static void main(String[] args){
+    System.out.println(fibo(50));
+  }
+
+}

1e0aab85086d48e0317568ce1c12ba22c15b4e52
sw-develop <psw00719@naver.com>
2022-02-20 13:38:42 +0000

    문제 이름 : N 찍기

---
+++ BOJ/Main2741.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        
+        int N = kbd.nextInt();
+        
+        for(int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+          
+    }
+}

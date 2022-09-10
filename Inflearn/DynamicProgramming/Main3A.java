cb99f2efe8733c80ff59ba405c4cb91f0cc8e782
sw-develop <psw00719@naver.com>
2022-09-10 13:29:46 +0000

    dp

---
+++ Inflearn/DynamicProgramming/Main3A.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main3A {
+
+  public static int solution(int N) {
+    int[] dy = new int[N + 1];
+    dy[1] = 1;
+    dy[2] = 2;
+    for (int i = 3; i <= N; i++) {
+      dy[i] = dy[i-2] + dy[i-1];
+    }
+    return dy[N];
+  }
+
+  public static void main(String[] args) {
+    Scanner kbd = new Scanner(System.in);
+    int N = kbd.nextInt();
+    System.out.println(solution(N));
+  }
+}

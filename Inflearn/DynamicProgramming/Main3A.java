d5c32dfce6f2116912b9854ad8aee8ba8ad39839
sw-develop <psw00719@naver.com>
2022-09-12 01:47:02 +0000

    dp - bottomup

--- Inflearn/DynamicProgramming/Main3A.java
+++ Inflearn/DynamicProgramming/Main3A.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class Main3A {
 
   public static int solution(int N) {
-    int[] dy = new int[N + 1];
+    int[] dy = new int[N + 2];
     dy[1] = 1;
     dy[2] = 2;
-    for (int i = 3; i <= N; i++) {
+    for (int i = 3; i <= N + 1; i++) {
       dy[i] = dy[i-2] + dy[i-1];
     }
-    return dy[N];
+    return dy[N + 1];
   }
 
   public static void main(String[] args) {

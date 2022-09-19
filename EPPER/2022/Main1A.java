a6932a561eca39d91ff0ec606e8a4a54a2e96207
sw-develop <psw00719@naver.com>
2022-09-19 00:34:29 +0000

    dp - 오르막수

--- EPPER/2022/Main1A.java
+++ EPPER/2022/Main1A.java
@@ -1,31 +1,34 @@
-import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
-class Main {
-  public static double solution(int[] num_arr, int n) {
-    double sol = 0;
+public class Main1A {
 
-    // 1.오름차순 정렬
-    Arrays.sort(num_arr);
-
-    // 2.처음 2개 평균값 구하기
-    sol = (num_arr[0] + num_arr[1]) / (float) 2;
-
-    // 3.나머지 차례대로 더해서 평균값 구하기
-    for (int i = 2; i < n; i++) {
-      sol = (sol + num_arr[i]) / (float) 2;
+  public static final int MOD = 10007;
+  
+  public static int solution(int N) {
+    int[][] dp = new int[N+1][10];
+    //초기화
+    for (int i = 0; i < N+1; i++) {
+      Arrays.fill(dp[i], 1);
     }
-
-    return sol;
+    //dp 수행
+    for (int i = 2; i <= N; i++) {
+      for (int j = 1; j < 10; j++) {
+        dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        dp[i][j] %= MOD;
+      }
+    }
+    //값 반환
+    int answer = 0;
+    for (int i : dp[N]) {
+      answer += i;
+      answer %= MOD;
+    }
+    return answer;
   }
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine()); // 점수 개수 입력 변수
-    int[] num_arr = new int[n]; // 점수 값 배열
-    for (int i = 0; i < n; i++) {
-      num_arr[i] = Integer.parseInt(br.readLine());
-    }
-    System.out.printf("%.6f", solution(num_arr, n));
+  public static void main(String[] args) {
+    Scanner kbd = new Scanner(System.in);
+    int N = kbd.nextInt();
+    System.out.println(solution(N));
   }
 }

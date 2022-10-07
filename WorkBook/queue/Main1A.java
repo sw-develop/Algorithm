c764a156edd22abe38e432f4a4204ca0a6c55c71
sw-develop <psw00719@naver.com>
2022-10-07 14:59:58 +0000

    Queue - deque 사용

--- WorkBook/queue/Main1A.java
+++ WorkBook/queue/Main1A.java
@@ -12,45 +12,45 @@ public class Main1A {
     StringBuilder ans = new StringBuilder();
 
     for (int i = 0; i < N; i++) {
-      StringTokenizer command = new StringTokenizer(br.readLine());
+      StringTokenizer command = new StringTokenizer(br.readLine(), " ");
 
       switch (command.nextToken()) {
         case "push":
-          dq.offer(Integer.parseInt(command.nextTokne()));
+          dq.offer(Integer.parseInt(command.nextToken()));
           break;
 
         case "pop":
           if (!dq.isEmpty()) {
-            ans.append(dq.poll());
+            ans.append(dq.poll()).append('\n');
           } else {
-            ans.append("-1");
+            ans.append(-1).append('\n');
           }
           break;
 
         case "size":
-          ans.append(dq.size());
+          ans.append(dq.size()).append('\n');
           break;
 
         case "empty":
           if (dq.isEmpty())
-            ans.append("1");
+            ans.append(1).append('\n');
           else
-            ans.append("0");
+            ans.append(0).append('\n');
           break;
 
         case "front":
           if (!dq.isEmpty()) {
-            ans.append(dq.getFirst());
+            ans.append(dq.getFirst()).append('\n');
           } else {
-            ans.append("-1");
+            ans.append(-1).append('\n');
           }
           break;
 
         case "back":
           if (!dq.isEmpty()) {
-            ans.append(dq.getLast());
+            ans.append(dq.getLast()).append('\n');
           } else {
-            ans.append("-1");
+            ans.append(-1).append('\n');
           }
           break;
 

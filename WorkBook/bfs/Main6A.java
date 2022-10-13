6c6edac2ca091d2e9db0ad34993871a8f9cfcb05
sw-develop <psw00719@naver.com>
2022-10-13 23:27:35 +0000

    BFS 활용

--- WorkBook/bfs/Main6A.java
+++ WorkBook/bfs/Main6A.java
@@ -1,10 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main6B {
-
-  static int[] dx = { -1, 1, 0, 0 }; // 상하좌우
-  static int[] dy = { 0, 0, -1, 1 };
+public class Main {
 
   static int N, M;
   static char[][] map;
@@ -12,51 +9,55 @@ public class Main6B {
   //dist[x][y][0] : 벽을 하나도 부수지 않고 (x, y)까지 오는데 걸리는 비용
   //dist[x][y][1] : 벽을 하나만 부수고 (x, y)까지 오는데 걸리는 비용
 
+  static int[] dx = {-1, 1, 0, 0};
+  static int[] dy = {0, 0, -1, 1};
+
   public static int solution() {
-    for (int i = 0; i < N; i++) { //dist 초기화
+    dist = new int[N][M][2];
+    for (int i = 0; i < N; i++) {
       for (int j = 0; j < M; j++) {
         dist[i][j][0] = dist[i][j][1] = -1;
       }
     }
 
-    dist[0][0][0] = dist[0][0][1] = 1;
+    dist[0][0][0] = 1;
+    Queue<int[]> q = new LinkedList<>(); //좌표(x,y)와 벽 부순 횟수 저장
+    q.offer(new int[]{0, 0, 0});
 
-    Queue<int[]> q = new LinkedList<>(); //이동 가능한 좌표 저장
-    q.offer(new int[]{0,0,0});
-    while (!q.isEmpty()) {
+    while (!q.isEmpty()) { //BFS
       int[] tmp = q.poll();
       int x = tmp[0];
       int y = tmp[1];
       int broken = tmp[2];
       if (x == N-1 && y == M-1) return dist[x][y][broken];
-      
+
       int nextDist = dist[x][y][broken] + 1;
-      for (int dir = 0; dir < dx.length; dir++) {
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
+      for (int idx = 0; idx < dx.length; idx++) {
+        int nx = x + dx[idx];
+        int ny = y + dy[idx];
         if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-        if (map[nx][ny] == '0' && dist[nx][ny][broken] == -1) { //이동 가능한 곳이고 아직 방문 X
-          dist[nx][ny][broken] = nextDist;
+        //벽이 아니고 아직 방문 X
+        if (map[nx][ny] == '0' && dist[nx][ny][broken] == -1) {
           q.offer(new int[]{nx, ny, broken});
+          dist[nx][ny][broken] = nextDist;
         }
-        if (broken == 0 && map[nx][ny] == '1' && dist[nx][ny][1] == -1) { //벽이고, 아직 부순 적 없고, 아직 방문 X
-          dist[nx][ny][1] = nextDist;
+        //아직 벽을 부순 적 없고, 벽이고, 아직 방문 X
+        if (broken == 0 && map[nx][ny] == '1' && dist[nx][ny][1] == -1) {
           q.offer(new int[]{nx, ny, 1});
+          dist[nx][ny][1] = nextDist;
         }
       }
     }
     return -1;
   }
-
+  
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-    
     map = new char[N][M];
-    dist = new int[N][M][2];
     for (int i = 0; i < N; i++) {
       String tmp = br.readLine();
       for (int j = 0; j < M; j++) {
@@ -67,3 +68,10 @@ public class Main6B {
     System.out.println(solution());
   }
 }
+
+/*
+문제 회고
+
+좌표까지 도달할 수 있는 경우가 여러가지 일 때 3차원 배열 활용하기
+
+*/

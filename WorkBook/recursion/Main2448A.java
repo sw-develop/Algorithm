e943256d9d69f483bcd303d5979084f6103daf44
sw-develop <psw00719@naver.com>
2022-12-05 00:39:18 +0000

    재귀, 2차원배열

---
+++ WorkBook/recursion/Main2448A.java
@@ -0,0 +1,56 @@
+import java.io.*;
+
+public class Main2448A {
+
+  static char[][] board;
+
+  public static void fillStar(int x, int y) {
+    board[x][y] = '*';
+    board[x+1][y-1] = '*'; board[x+1][y+1] = '*';
+    for (int i = y - 2; i <= y + 2; i++) {
+      board[x+2][i] = '*';
+    }
+  }
+
+  public static void solution(int l, int x, int y) {
+    // 재귀 종료
+    if (l == 3) {
+      fillStar(x, y);
+      return;
+    }
+    // 재귀 수행
+    int nl = l / 2;
+    solution(nl, x, y);
+    solution(nl, x + nl, y - nl);
+    solution(nl, x + nl, y + nl);
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    board = new char[N][2*N - 1];
+    solution(N, 0, N-1);
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < 2*N - 1; j++) {
+        if (board[i][j] == '*') sb.append('*');
+        else sb.append(' ');
+      }
+      sb.append('\n');
+    }
+    System.out.println(sb);
+  }
+}
+
+/* 문제 회고
+
+<놓친 부분>
+재귀 종료, 재귀 수행 조건 생각하기(=> 재귀 단위 떠올리기)
+
+<풀이>
+값 저장 데이터 형식 - 배열 사용
+값 저장 방식 - 재귀 사용
+
+*/
